47476860821e9d05bffe33f08eecffdb427a3d88
tsr <tsrmix@gmail.com>
2018-09-27 19:10:44 +0900

    stash

--- sh.js
+++ sh.js
@@ -7,6 +7,15 @@ const {
 const path = require('path');
 const homeDirPath = require('os').homedir();
 const confDirPath = path.join(homeDirPath, '.idbt');
+const confFilePath = path.join(confDirPath, 'config');
+const draftFilePath = path.join(confDirPath, 'draft.md');
+const fs = require('fs');
+const {
+  open,
+  close
+} = fs;
+const fopen = p(open);
+const fclose = p(close);
 const mkdirp = p(require('mkdirp'));
 
 require('yargs')
@@ -19,6 +28,15 @@ require('yargs')
       mkdirp(confDirPath)
         .then(() => {
           console.log('mkdir ok! :', confDirPath);
+          // まず読み込みモードで開く。失敗する→存在しない
+          fopen(confFilePath, 'r')
+            .then((fd) => {
+              console.log('already exists', fd);
+            })
+            .catch(() => {
+              console.log('');
+            })
+
         }).catch((e) => {
           console.log('init failed!');
           throw e
